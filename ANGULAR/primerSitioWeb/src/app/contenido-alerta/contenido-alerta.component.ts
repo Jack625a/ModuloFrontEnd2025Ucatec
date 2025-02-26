fab96e30ab6a2fc61d31b8969cb79ce4a4073d00
Jack625a <ktfut15@gmail.com>
2025-02-25 21:37:40 -0400

    Contenido de una pantalla emergente

--- ANGULAR/primerSitioWeb/src/app/contenido-alerta/contenido-alerta.component.ts
+++ ANGULAR/primerSitioWeb/src/app/contenido-alerta/contenido-alerta.component.ts
@@ -1,11 +1,23 @@
-import { Component } from '@angular/core';
+import { Component,inject } from '@angular/core';
+import {
+  MatDialog,
+  MatDialogModule,
+  MatDialogActions,
+  MatDialogClose,
+  MatDialogContent,
+  MatDialogRef,
+  MatDialogTitle
+} from'@angular/material/dialog';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-contenido-alerta',
-  imports: [],
+  imports: [MatButtonModule,
+    MatDialogActions,MatDialogClose,MatDialogContent,MatDialogTitle],
   templateUrl: './contenido-alerta.component.html',
   styleUrl: './contenido-alerta.component.css'
 })
 export class ContenidoAlertaComponent {
+  readonly dialogoref=inject(MatDialogRef<ContenidoAlertaComponent>)
 
 }
