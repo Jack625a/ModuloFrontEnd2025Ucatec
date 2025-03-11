50f478c4fae11e20dfd978f46ada2cb0f9c6a1bb
Jack625a <ktfut15@gmail.com>
2025-03-10 20:44:55 -0400

    Componentes basicos React

---
+++ REACT/componentes/App.js
@@ -0,0 +1,56 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View,Image, Button,Alert,TextInput } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text 
+        style={styles.titulos}
+      >Kevin Arroyo</Text>
+      <Text style={styles.titulos}>Primer Sitio Web en React</Text>
+      <Image
+        source={{
+          uri:'https://talent500.com/blog/wp-content/uploads/sites/42/2024/05/react-must-be-in-scope-when-using-jsx-scaled-1.jpg'
+        }}
+        style={styles.imagenes}
+      />
+      <Button
+        title='Enviar'
+        onPress={()=> Alert.alert('Mostrando la alerta del click')}
+      />
+      <TextInput 
+        style={styles.entradaTexto}
+        placeholder='Ingrese su nombre: '
+        keyboardType='numeric'
+      />
+
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  titulos:{
+    fontSize:30,
+    color:'red',
+    fontFamily:'Cambria',
+    fontWeight:'bold'
+  },
+  imagenes:{
+    width:200,
+    height:200,
+    borderRadius:20,
+    padding:10,
+    margin:10
+  },
+  entradaTexto:{
+    padding:15,
+    borderWidth:1
+  }
+});
